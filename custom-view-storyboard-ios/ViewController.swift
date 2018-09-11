54605289ffb458ade2e91f14151016b0801b685d
Anderson Costa <anderson.analistati@gmail.com>
2018-09-11 20:36:42 -0300

    Initial commit

---
+++ custom-view-storyboard-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  custom-view-storyboard-ios
+//
+//  Created by Anderson de Carvalho on 11/09/18.
+//  Copyright © 2018 Anderson de Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
